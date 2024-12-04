8ac4c208536e78da3632e33ae45ed021fc7a69bd
Olaoluwa221 <oifeiji@emich.edu>
2024-12-04 01:43:32 -0500

    Generate DB scripts

---
+++ InsertIntoRoom.sql
@@ -0,0 +1,86 @@
+USE [IS645Project]
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (103, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (105, NULL)
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (107, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (111, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (112, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (114, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (115, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (116, N'Q')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (117, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (118, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (119, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (120, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (121, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (122, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (123, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (124, N'Q')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (125, N'Q')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (201, N'Q')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (202, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (203, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (204, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (205, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (206, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (207, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (208, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (209, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (210, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (211, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (212, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (213, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (214, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (215, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (216, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (217, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (218, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (219, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (220, N'K')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (221, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (222, N'Q')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (223, N'QQ')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (224, N'Q')
+GO
+INSERT [Motel].[Room] ([RoomNumber], [RoomType]) VALUES (225, N'Q')
+GO
